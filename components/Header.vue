68a1fe571afcd22f4004e7c7d071a5a53220a91f
26radit <144081703+26radit@users.noreply.github.com>
2024-06-10 13:42:49 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+            <img src="~/assets/img/LOGO-SMK4.png" alt="logo smkn4" height="100px" width="100px">
+        </div>
+        <div class="col-lg-11">
+            <h1>Perpustakaan Digital</h1>
+            <address>
+                SMKN 4 Tasikmalaya
+                <p>JL.Depok, Sukamenak, purbaratu</p>
+            </address>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+              <h2>Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+  
+  <div class="statistik">
+      <h1>STATISTIK</h1>
+    </div>
+
+    <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+          <div class="card rounded-5 color1">
+            <div class="card-body d-flex justify-content-around d-flex align-items-center">
+              <h2><span class="no">{{ jml_pengunjung }}</span> pengunjung</h2>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+          
+            <div class="card rounded-5 color2">
+              <div class="card-body d-flex justify-content-around d-flex align-items-center">
+                <h2><span class="no">{{ jml_buku }}</span> Buku</h2>
+              </div>
+            </div>            
+        </div>
+      </div>
+      <div class="line">
+        <statistik/>
+      </div>
+    </div>
+</template>
+
+<script setup>
+const supabase = useSupabaseClient()
+const jml_pengunjung = ref(0)
+const jml_buku = ref(0)
+
+async function getjml_pengunjung() {
+  const{ error , data, count } = await supabase
+  .from("pengunjung")
+  .select('*', { count: 'exact' })
+  if (count) jml_pengunjung.value = count
+  
+}
+async function getjml_buku() {
+  const{ error , data, count } = await supabase
+  .from("Buku")
+  .select('*', { count: 'exact' })
+  if (count) jml_buku.value = count
+  
+}
+
+
+onMounted(() => {
+  getjml_pengunjung()
+  getjml_buku()
+})
+</script>
+
+
+<style scoped>
+.no{
+  font-size: 60px;
+}
+.color1 {
+  background-color: #157aee;
+}
+.color2 {
+  background-color: #157aee;
+}
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 10px #000000;
+}
+.card.bg-pengunjung {
+  background-image: url('../assets/img/pengunjung.png');
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+.card.bg-buku {
+  background: url(../assets/img/buku.jpeg) no-repeat center center;
+  background-size: cover;
+}
+</style>
